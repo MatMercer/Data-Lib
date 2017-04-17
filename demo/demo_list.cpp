af43380999e8515ece0aeec5ae685d7c1edc2882
MtMercer <mateusmercer@gmail.com>
2017-04-17 06:26:19 -0300

    Enhance demo_list

--- demo/demo_list.cpp
+++ demo/demo_list.cpp
@@ -6,9 +6,27 @@ using namespace std;
 int main() {
     List<int> list = List<int>(2);
 
-    for (int i = 0; i < 1000; i++) {
-        list.add(i, i * 2);
+    for (int i = 1; i <= 10; i++) {
+        list.add(i - 1, i * 2);
     }
+
+    cout << "Current list:" << endl;
+    list.printList();
+
+
+    list.add(5, -1);
+    cout << "Current list with -1 added to index 5:" << endl;
+    list.printList();
+
+    list.add(7, -1);
+    cout << "Current list with -1 added to index 7:" << endl;
+    list.printList();
+
+    for (int i = 3; i < 13; i++) {
+        list.add(i, -1);
+    }
+    cout << "Added ten -1 elements with the 4 index as a start..." << endl;
+    list.printList();
 
     return 0;
 }
